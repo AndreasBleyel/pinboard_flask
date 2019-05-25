b9a5a6725cae02e480a873f9f47feb39ad8e039d
andy-b23 <andy-b23@gmx.at>
2019-05-25 11:26:54 +0200

    ue2 fertig. DB working

--- pinboard/board.py
+++ pinboard/board.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from pinboard.db import get_db
 
 bp = Blueprint("board", __name__)
 
 @bp.route("/")
 def list():
-    return render_template("board/list.html")
+    db = get_db()
+    posts = db.execute("SELECT * FROM post ORDER BY created DESC").fetchall()
+    return render_template("board/list.html", posts=posts)
 
 @bp.route("/add", methods=("GET", "POST"))
 def add():
-    return render_template("board/add.html")
+    if request.method == "GET":
+        return render_template("board/add.html")
+    else:
+        title = request.form["title"]
+        desc = request.form["description"]
+        color = request.form["color"]
+
+        db = get_db()
+        db.execute("INSERT INTO post (title, description, color) VALUES (?,?,?)", (title, desc, color))
+        db.commit()
+
+        return redirect(url_for("board.list"))
---
+++ pinboard/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS post;
+
+create table post (
+  id integer primary key autoincrement,
+  created timestamp not null default current_timestamp ,
+  title text not null ,
+  description text not null ,
+  color text not null
+);
